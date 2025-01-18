6632215ec734303a957f157aebe95b686cc05927
TravisWood42 <traviswoodlasalle@gmail.com>
2025-01-18 11:57:06 -0500

    Finished AK Instal
    Added all necessary code for AdvantageKit.

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "GitHub";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "f34e070e9929cc3f8718cb8816eb52a844e826f2";
+  public static final String GIT_DATE = "2025-01-11 15:49:04 EST";
+  public static final String GIT_BRANCH = "AdvantageKit";
+  public static final String BUILD_DATE = "2025-01-18 11:42:34 EST";
+  public static final long BUILD_UNIX_TIME = 1737218554953L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}

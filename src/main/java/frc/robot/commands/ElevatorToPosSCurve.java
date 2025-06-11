4b71fbcd7044a71ed59abcb4bb6ef9d6e6878ec8
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2025-06-11 10:17:47 -0400

    gardleRIO update and s curve elvator

---
+++ src/main/java/frc/robot/commands/ElevatorToPosSCurve.java
@@ -0,0 +1,88 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.Constants.ElevatorConstants;
+
+import frc.robot.utils.SCurveMotionProfile;
+import frc.robot.utils.SCurveMotionProfile.State;
+
+public class ElevatorToPosSCurve extends Command {
+    private final ElevatorSubsystem m_Elevator;
+    private final PIDController m_PID;
+    private final ElevatorFeedforward m_FF;
+    private final SCurveMotionProfile m_Profile;
+    private final double targetPos;
+
+    private double startT;
+
+    public ElevatorToPosSCurve(ElevatorSubsystem elevator, double targetPosition, double maxVelocity,
+            double maxAcceleration, double maxJerk) {
+        m_Elevator = elevator;
+        targetPos = targetPosition;
+
+        m_PID = new PIDController(ElevatorConstants.kP, ElevatorConstants.kI, ElevatorConstants.kD);
+        m_PID.setTolerance(ElevatorConstants.elevatorPosTolerance);
+
+        m_FF = new ElevatorFeedforward(ElevatorConstants.kS, ElevatorConstants.kG, ElevatorConstants.kV, ElevatorConstants.kA);
+
+        double currentPos = m_Elevator.getPosition();
+        double currentVel = 0; // add method to subsytem latter
+
+        m_Profile = new SCurveMotionProfile(
+                currentPos, currentVel,
+                targetPos, 0,
+                maxVelocity, maxAcceleration, maxJerk);
+
+        addRequirements(m_Elevator);
+    }
+
+    @Override
+    public void initialize() {
+        startT = Timer.getFPGATimestamp();
+    }
+
+    @Override
+    public void execute() {
+        double elapsedTime = Timer.getFPGATimestamp() - startT;
+
+        double dt = 0.02; // 50 hz from command schduler
+
+        State currentState = m_Profile.sample(elapsedTime);
+        State nextState = m_Profile.sample(elapsedTime + dt);
+
+        m_PID.setSetpoint(currentState.position);
+
+        double pidOutput = m_PID.calculate(m_Elevator.getPosition());
+
+        double feedforwardVoltage = m_FF.calculateWithVelocities(currentState.velocity, nextState.velocity);
+
+        double voltage = pidOutput + feedforwardVoltage;
+
+        voltage = MathUtil.clamp(voltage, -ElevatorConstants.clampRangeForSpeed, ElevatorConstants.clampRangeForSpeed);
+
+        m_Elevator.setElevatorVoltage(voltage);
+
+        // SmartDashboard.putNumber("SCurve Target Pos", currentState.position);
+        // SmartDashboard.putNumber("SCurve Target Vel", currentState.velocity);
+        // SmartDashboard.putNumber("SCurve Target Accel", currentState.acceleration);
+        // SmartDashboard.putNumber("Elevator Voltage", voltage);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_Elevator.setElevatorVoltage(ElevatorConstants.elevatorHoldVoltage);
+    }
+
+    @Override
+    public boolean isFinished() {
+        double elapsedTime = Timer.getFPGATimestamp() - startT;
+
+        return elapsedTime >= m_Profile.getTotalTime() && m_PID.atSetpoint();
+    }
+}
